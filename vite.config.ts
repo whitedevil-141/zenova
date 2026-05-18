b4289d95fbd058145281fc471383e71d04a01562
WHITE DEVIL <white.devil.dev.141@gmail.com>
2026-05-18 19:08:40 +0600

    set GITHUB_PAGES environment variable for build process and update base path in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 import path from 'node:path';
 
 export default defineConfig({
+  base: process.env.GITHUB_PAGES ? '/zenova/' : '/',
   plugins: [react()],
   resolve: {
     alias: {
